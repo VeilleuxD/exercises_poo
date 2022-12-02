98568e66e97c6147a11c3bcd60a457ef46e9cdf6
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-12-02 16:17:29 -0500

    exercise 2 et debut exercise 3

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 david A. V. gr 405
 exercise 1
 """
+from math import pi
+import random
+
 print("exercise 1")
 
 
@@ -44,7 +47,6 @@ livre.afficher_info()
 
 # exercise 3: le cercle
 
-from math import pi
 
 print("\n\n\nexercise 3")
 
@@ -53,8 +55,42 @@ class Cercle:
 
     def __init__(self, rayon):
         self.rayon = rayon
-        self.circonference = None
-        self.aire = None
+        self.circonference_cercle = None
+        self.aire_cercle = None
 
-    def circonference(self):
-        self.circonference = 55
+    def calc_autour(self):
+        self.circonference_cercle = 2 * pi * int(self.rayon)
+        print(f"la circonférence est {self.circonference_cercle}")
+
+    def calc_air(self):
+        self.aire_cercle = self.rayon ** 2 * pi
+
+        print(f"l'aire du cercle est {self.aire_cercle}")
+
+
+assiette = Cercle(7)
+
+print(f"le rayon est {assiette.rayon}")
+assiette.calc_air()
+
+assiette.calc_autour()
+
+# exercise 4
+
+print("\n\n\nexercise 4")
+
+
+class Hero:
+
+    def __init__(self, hp, attack_power, defence, nom):
+        self.hp = hp
+        self.attack_power = attack_power
+        self.defence = defence
+        self.nom = nom
+
+    def attaquer(self):
+        degat = self.attack_power + random.randint(1, 6)
+
+        print(f"vous attaquez pour {degat} dégats")
+
+
