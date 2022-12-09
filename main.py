d2dbc28da1a99bc618b34d28a89b6f32f2e668a3
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-12-09 10:16:53 -0500

    Fin du TP4, exercises 1 a 6

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ print("\n\n\nexercise 4")
 
 class Hero:
 
-    def __init__(self, hp, attack_power, defence, nom, est_vivant):
+    def __init__(self, nom):
         self.est_vivant = True
         self.hp = random.randint(1, 10) + random.randint(1, 10)
         self.attack_power = random.randint(1, 6)
@@ -113,8 +113,7 @@ class Hero:
             self.est_vivant = True
 
 
-bernard = Hero(random.randint(1, 10) + random.randint(1, 10),
-               random.randint(1, 6), random.randint(1, 6), 'Bernard', True)
+bernard = Hero('Bernard')
 
 print(
     f"il a {bernard.hp} hp.\nson nom est {bernard.nom}\n"
@@ -139,3 +138,42 @@ class AbilityScore:
     sag = random.randint(1, 20)
     cha = random.randint(1, 20)
 
+
+# EXERCISE 6
+print("EXERCISE 6 (final)")
+
+
+class HeroExerciseSix:
+    nom: str
+    str = random.randint(1, 20)
+    dex = random.randint(1, 20)
+    con = random.randint(1, 20)
+    int = random.randint(1, 20)
+    sag = random.randint(1, 20)
+    cha = random.randint(1, 20)
+    est_vivant = True
+    hp = random.randint(1, 10) + random.randint(1, 10)
+    attack_power = random.randint(1, 6)
+    defence = random.randint(1, 6)
+
+    def attaquer(self):
+        degat = self.attack_power + random.randint(1, 6)
+
+        print(f"{self.nom} attaque pour {degat} dégats!")
+
+    def endommage(self):
+        dommage = random.randint(1, 6)
+
+        if dommage - self.defence < 0:
+            print(f"{self.nom} recoit 0 dégats")
+        else:
+            self.hp -= dommage - self.defence
+            print(f"{self.nom} recoit {dommage - self.defence} dégats")
+
+    def vivant_check(self):
+        if self.hp <= 0:
+            print(f"{self.nom} est pas vivant")
+            self.est_vivant = False
+        else:
+            print(f"{self.nom} est vivant")
+            self.est_vivant = True
